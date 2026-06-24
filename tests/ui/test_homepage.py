d119048f8cf04dcf9a9a433801a0fac7122fc1d4
Lekha-SDET <150242081+lekhasri-web@users.noreply.github.com>
2026-06-24 22:12:26 +0530

    Refactor homepage test to use default URL
    Updated open_url method call to use default URL.

--- tests/ui/test_homepage.py
+++ tests/ui/test_homepage.py
@@ -3,6 +3,6 @@ from pages.home_page import HomePage
 
 def test_homepage_search_bar(human_page_fixture):
     home_page = HomePage(human_page_fixture)
-    home_page. open_url("https://automationexercise.com/")
+    home_page. open_url()
     expect(home_page.heading()).to_be_visible(timeout=5000)
     expect(home_page.page).to_have_title("Automation Exercise")

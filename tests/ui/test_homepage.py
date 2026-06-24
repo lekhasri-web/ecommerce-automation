6e77d552f4382cab0bb5b96fe79fe794cd9a0cda
Lekha-SDET <150242081+lekhasri-web@users.noreply.github.com>
2026-06-24 22:15:21 +0530

    Rename test and update visibility timeout

--- tests/ui/test_homepage.py
+++ tests/ui/test_homepage.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import Page,expect
 from pages.home_page import HomePage
 
-def test_homepage_search_bar(human_page_fixture):
+def test_homepage_loads_successfully(human_page_fixture):
     home_page = HomePage(human_page_fixture)
     home_page. open_url()
-    expect(home_page.heading()).to_be_visible(timeout=5000)
+    expect(home_page.heading()).to_be_visible()
     expect(home_page.page).to_have_title("Automation Exercise")

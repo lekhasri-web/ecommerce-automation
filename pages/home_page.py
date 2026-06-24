db51aafb0f905d5f7dc23b86069e2c51896f1f88
Lekha-SDET <150242081+lekhasri-web@users.noreply.github.com>
2026-06-24 22:11:40 +0530

    Modify open_url method to use a fixed URL

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,8 +4,8 @@ class HomePage:
     def __init__(self, page):
         self.page = page
     
-    def open_url(self, url: str):
-        self.page.goto(url)
+    def open_url(self):
+        self.page.goto("https://automationexercise.com/")
 
     def heading(self):
         return self.page.get_by_role("heading",name="Full-Fledged practice website for Automation Engineers")

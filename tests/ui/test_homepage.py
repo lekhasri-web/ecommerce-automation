976e85de88610f00b9049859efccb8beda8ec555
Lekha-SDET <150242081+lekhasri-web@users.noreply.github.com>
2026-06-24 22:16:32 +0530

    Refactor test_homepage_loads_successfully function

--- tests/ui/test_homepage.py
+++ tests/ui/test_homepage.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import Page,expect
 from pages.home_page import HomePage
 
-def test_homepage_loads_successfully(human_page_fixture):
-    home_page = HomePage(human_page_fixture)
-    home_page. open_url()
+def test_homepage_loads_successfully(page):
+    home_page = HomePage(page)
+    home_page.open_url()
     expect(home_page.heading()).to_be_visible()
     expect(home_page.page).to_have_title("Automation Exercise")

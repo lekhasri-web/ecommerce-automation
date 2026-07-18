44ab880ceaba4553b4fe158432741a4b29d76402
Lekha sri <lsri4739@gmail.com>
2026-07-18 16:06:25 +0530

    ci.yml fix

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 BASE_URL = "https://automationexercise.com"
 TEST_EMAIL = os.getenv("TEST_EMAIL", "claude@gmail.com")
-TEST_PASSWORD = os.getenv("TEST_PASSWORD", "")
+TEST_PASSWORD = os.getenv("TEST_PASSWORD", "Test@1234")
